7efe7ba01f9b363935938eac8c24ed31d5a69d3c
Bogdan Dobrica <bdobrica@gmail.com>
2023-05-05 22:42:45 +0300

    Update test_model_service.py

--- containers/mlops/tests/test_model_service.py
+++ containers/mlops/tests/test_model_service.py
@@ -7,7 +7,7 @@ K8SConfig.load_incluster_config()
 def test_model_service_create():
     model_service = ModelService(name="titanic-rfc", namespace="titanic").create()
     assert model_service.body.spec.selector["model"] == "titanic-rfc"
-    assert model_service.body.spec.ports[0].port == 80
+    assert model_service.body.spec.ports[0].port == 8080
     assert model_service.body.spec.ports[0].target_port == 8080
     assert model_service.body.spec.ports[0].protocol == "TCP"
 
