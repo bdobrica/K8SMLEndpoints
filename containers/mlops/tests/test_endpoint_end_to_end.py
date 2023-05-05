2622a6b345df1a8abae35a1fc2087189da89d330
Bogdan Dobrica <bdobrica@gmail.com>
2023-05-05 22:56:59 +0300

    Create test_endpoint_end_to_end.py

---
+++ containers/mlops/tests/test_endpoint_end_to_end.py
@@ -0,0 +1,30 @@
+from kubernetes import config as K8SConfig
+from resources import Endpoint, EndpointConfig, Model
+
+
+def test_endpoint_config_create():
+    model = Model(name="titanic-rfc", namespace="titanic").create(
+        image="quay.io/bdobrica/ml-operator-tools:model-latest",
+        artifact="https://ublo.ro/wp-content/friends/titanic.tar.gz",
+    )
+    endpoint_config = EndpointConfig(name="titanic-rfc", namespace="titanic").create(
+        models=[
+            {
+                "model": "titanic-rfc",
+                "weight": 42,
+                "cpus": "100m",
+                "memory": "100Mi",
+                "instances": 2,
+                "size": "1Gi",
+                "path": "/mnt/nfs/models",
+            }
+        ]
+    )
+    endpoint = (
+        Endpoint(name="titanic-rfc", namespace="titanic")
+        .create(
+            endpoint_config="titanic-rfc",
+            host="titanic-rfc.titanic.svc.cluster.local",
+        )
+        .create_handler()
+    )
