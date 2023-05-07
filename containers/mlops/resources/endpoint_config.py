5b1566a0df158e077feaef1f0bd3e95b3aac7e66
Bogdan Dobrica <bdobrica@gmail.com>
2023-05-07 18:13:01 +0300

    Update endpoint_config.py

--- containers/mlops/resources/endpoint_config.py
+++ containers/mlops/resources/endpoint_config.py
@@ -101,6 +101,7 @@ class EndpointConfig:
         model_versions: Optional[List[str]] = None,
         state: Optional[str] = None,
     ):
+        print("cloning endpoint config")
         return EndpointConfig(
             name=self.name,
             namespace=self.namespace,
