42a51fd80c0ed0b90c94c40f2abb6f4a12a53eb0
Bogdan Dobrica <bdobrica@gmail.com>
2023-05-09 21:52:15 +0300

    Documentation and some containers

--- README.md
+++ README.md
@@ -2,7 +2,23 @@
 
 Repository that tries to show how to implement SageMaker like endpoints in Kubernetes.
 
-## Goals
+# Repo structure
+
+* `container`: Contains the code for the container that will be deployed in Kubernetes
+    * `fluentbit`: Container that retrieves CPU, MEM and IO stats from the pod
+    * `goreplay`: Container that captures traffic from the pod
+    * `mlops`: Container that contains the Machine Learning kubernetes operator pod
+        * `resources`: High level kubernetes resources abstraction
+        * `tests`: pytest tests for the resources
+        * `utils`: Utilities for managing kubernetes resources (eg. versioning)
+        * `mlops.py`: Main file that contains the Machine Learning kubernetes operator
+    * `model`: Container that contains the model serving pod, based on Titanic toy model
+    * `model-init`: Container that contains the artifact downloader and unpacker pod
+    * `nginx`: Container that contains the reverse proxy pod
+* `k8s`: Contains the Kubernetes manifests to deploy the container in Kubernetes
+* `server`: Contains scripts to configure Kubernetes
+
+# Goals
 
 * Provide a simple way to deploy ML models in Kubernetes
 * Provide a simple way to swap models under the same endpoint in Kubernetes
--- containers/mlops/resources/endpoint_config.py
+++ containers/mlops/resources/endpoint_config.py
@@ -7,7 +7,30 @@ from utils import DiffLine, DiffLineType, get_version
 
 
 class EndpointConfig:
-    def __init__(self, name: str, namespace: str = "default", version: str = ""):
+    """
+    The EndpointConfig class is a handling model configuration for an endpoint. It is used to create, update, and delete
+    endpoint configs. It is also used to retrieve information about the endpoint config and the models associated with
+    it.
+
+    An endpoint config allows specifying multiple models to be used for an endpoint by providing for each the weight of
+    the traffic to be routed to it. An also allows specifying each model's resource requirements and the number of
+    replicas to be used for each model.
+
+    The underlying Kubernetes resource used for the endpoint config is an Istio VirtualService that provides the routing
+    information for the endpoint.
+    """
+
+    def __init__(self, name: str, namespace: str = "default", version: str = "") -> None:
+        """
+        Initialize an EndpointConfig object by providing a name and namespace. If the endpoint config already exists,
+        the information describing it is retrieved from the API and stored in the body attribute. If the endpoint config
+        does not exist, the body attribute is set to None.
+
+        :param name: The name of the endpoint config.
+        :param namespace: The namespace of the endpoint config. Defaults to the "default" namespace.
+        :param version: The version of the endpoint config. If the version is provided, the name is set to the
+        name-version format. This is used when creating a new instance.
+        """
         self.name: str = name
         self.namespace: str = namespace
         self.version: str = version
@@ -26,6 +49,14 @@ class EndpointConfig:
         self.virtual_service = IstioVirtualService(self.virtual_service_name, self.namespace)
 
     def get_models(self, models: List[Dict[str, str]] = None) -> List[Model]:
+        """
+        Get the models associated with the endpoint config. If the endpoint config does not exist, an empty list is
+        returned. You can optionally provide a list of models to use instead of the models associated with the endpoint
+        config.
+
+        :param models: A list of models to use instead of the models associated with the endpoint config.
+        :return: A list of Model objects.
+        """
         if not self.body:
             return []
 
@@ -48,6 +79,17 @@ class EndpointConfig:
         model_versions: Optional[List[str]] = None,
         state: Optional[str] = None,
     ) -> MLOpsClient.V1Alpha1EndpointConfig:
+        """
+        Method used to create the Kubernetes API request body for creating/updating an endpoint config.
+
+        :param models: A list of models to use instead of the models associated with the endpoint config. Each model is
+        a dictionary with the keys "model", "weight", "cpus", "memory", and "instances".
+        :param endpoint: The name of the endpoint that the endpoint config is associated with.
+        :param model_versions: A list of model versions for each model provided in the models parameter. The order of
+        the model versions must match the order of the models.
+        :param state: The state of the endpoint config.
+        :return: A Kubernetes API request body for creating/updating an endpoint config.
+        """
         return MLOpsClient.V1Alpha1EndpointConfig(
             metadata=MLOpsClient.V1Alpha1ObjectMeta(
                 name=self.named_version,
@@ -69,7 +111,11 @@ class EndpointConfig:
             ),
         )
 
-    def get_endpoint(self) -> MLOpsClient.V1Alpha1Endpoint:
+    def get_endpoint(self) -> Optional[MLOpsClient.V1Alpha1Endpoint]:
+        """
+        Get the endpoint associated with the endpoint config. If the endpoint config does not exist, None is returned.
+        :return: An Endpoint object.
+        """
         api = MLOpsClient.V1Alpha1Api()
 
         if self.body and self.body.status:
@@ -84,6 +130,19 @@ class EndpointConfig:
         model_versions: Optional[List[str]] = None,
         state: Optional[str] = None,
     ) -> "EndpointConfig":
+        """
+        Method used for creating the EndpointConfig associated kubernetes resource. The method does not create any
+        other kubernetes resources such as the endpoint or the virtual service. The method returns a reference to self.
+        For creating associated resources, use the create_handler method.
+
+        :param models: A list of models to use instead of the models associated with the endpoint config. Each model is
+        a dictionary with the keys "model", "weight", "cpus", "memory", and "instances".
+        :param endpoint: The name of the endpoint that the endpoint config is associated with.
+        :param model_versions: A list of model versions for each model provided in the models parameter. The order of
+        the model versions must match the order of the models.
+        :param state: The state of the endpoint config.
+        :return: An EndpointConfig object (reference to self for easy chaining).
+        """
         if self.body:
             return self
 
@@ -104,6 +163,19 @@ class EndpointConfig:
         model_versions: Optional[List[str]] = None,
         state: Optional[str] = None,
     ):
+        """
+        This method is intended on a kubernetes resource that already exists. It will create a new kubernetes resource
+        with the name of the current resource appended with a new version string. Parts of the original (self) resource
+        can be overridden by providing the appropriate parameters. It does not create any associated resources.
+
+        :param models: A list of models to use instead of the models associated with the endpoint config. Each model is
+        a dictionary with the keys "model", "weight", "cpus", "memory", and "instances".
+        :param endpoint: The name of the endpoint that the endpoint config is associated with.
+        :param model_versions: A list of model versions for each model provided in the models parameter. The order of
+        the model versions must match the order of the models.
+        :param state: The state of the endpoint config.
+        :return: An EndpointConfig object reference to the new resource.
+        """
         return EndpointConfig(
             name=self.name,
             namespace=self.namespace,
@@ -122,6 +194,19 @@ class EndpointConfig:
         model_versions: Optional[List[str]] = None,
         state: Optional[str] = None,
     ) -> "EndpointConfig":
+        """
+        Method used for updating the EndpointConfig associated kubernetes resource. The method does not update any
+        other kubernetes resources such as the endpoint or the virtual service. The method returns a reference to self.
+        For updating associated resources, use the update_handler method.
+
+        :param models: A list of models to use instead of the models associated with the endpoint config. Each model is
+        a dictionary with the keys "model", "weight", "cpus", "memory", and "instances".
+        :param endpoint: The name of the endpoint that the endpoint config is associated with.
+        :param model_versions: A list of model versions for each model provided in the models parameter. The order of
+        the model versions must match the order of the models.
+        :param state: The state of the endpoint config.
+        :return: An EndpointConfig object (reference to self for easy chaining).
+        """
         if not self.body or not self.body.status or not self.body.status.version:
             return self
 
@@ -138,6 +223,16 @@ class EndpointConfig:
         return self
 
     def delete(self) -> "EndpointConfig":
+        """
+        Method used for deleting the EndpointConfig associated kubernetes resource. The method does not delete any
+        other kubernetes resources such as the endpoint or the virtual service. The method returns a reference to self.
+        The return object will have the body attribute set to None.
+        Note: As you might probably don't want to delete the endpoint config without deleting the associated resources,
+        use add_finalizer before deleting the endpoint config and remove_finalizer after deleting the associated
+        resources.
+
+        :return: An EndpointConfig object (reference to self for easy chaining).
+        """
         if not self.body:
             return self
 
@@ -147,6 +242,13 @@ class EndpointConfig:
         return self
 
     def create_handler(self) -> "EndpointConfig":
+        """
+        Method used for creating associated resources. The method returns a reference to self.
+        An endpoint config has the following associated resources:
+        - an Istio virtual service
+
+        :return: An EndpointConfig object (reference to self for easy chaining).
+        """
         if self.body and self.body.status and self.body.status.model_versions:
             return self
 
@@ -191,7 +293,9 @@ class EndpointConfig:
         - if only weights are being updated, update the weights of the models on the virtual service;
         - if the number of models decreases, delete the models that are no longer needed;
         - if new models are swapped in or added, create new models, add them with the same weights to the virtual service, mark them for monitoring by the daemon, and when all good, delete the old models;
-        :param diff: The diff between the old and new versions of the CRD
+
+        :param diff: The diff between the old and new versions of the CRD as a list of DiffLine objects (see utils.py).
+        :return: An EndpointConfig object (reference to self for easy chaining).
         """
         endpoint = self.get_endpoint()
         if not endpoint:
@@ -251,6 +355,11 @@ class EndpointConfig:
         return self
 
     def delete_handler(self) -> "EndpointConfig":
+        """
+        Delete the EndpointConfig. Method intended to be used with kopf.on.delete.
+
+        :return: An EndpointConfig object (reference to self for easy chaining).
+        """
         for model in self.get_models():
             model.delete()
 
@@ -260,6 +369,14 @@ class EndpointConfig:
         return self
 
     def add_finalizers(self, finalizers: List[str]) -> "EndpointConfig":
+        """
+        Add finalizers to the EndpointConfig. While the finalizers are present, the EndpointConfig cannot be deleted
+        even if the deletion of the EndpointConfig is requested. When the finalizers are removed, the EndpointConfig
+        will be deleted.
+
+        :param finalizers: A list of finalizers to add (simple strings).
+        :return: An EndpointConfig object (reference to self for easy chaining).
+        """
         if isinstance(self.body.metadata.finalizers, list):
             for finalizer in finalizers:
                 if finalizer not in self.body.metadata.finalizers:
@@ -275,6 +392,14 @@ class EndpointConfig:
         return self
 
     def remove_finalizers(self, finalizers: List[str]) -> "EndpointConfig":
+        """
+        Remove finalizers from the EndpointConfig. While the finalizers are present, the EndpointConfig cannot be
+        deleted even if the deletion of the EndpointConfig is requested. When the finalizers are removed, the
+        EndpointConfig will be deleted.
+
+        :param finalizers: A list of finalizers to remove (simple strings).
+        :return: An EndpointConfig object (reference to self for easy chaining).
+        """
         if isinstance(self.body.metadata.finalizers, list):
             for finalizer in finalizers:
                 if finalizer in self.body.metadata.finalizers:
